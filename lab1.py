4a881a49d7406eeecb5f9daa1d03653b53cbb004
7bw <boylee99@hotmail.com>
2021-01-11 21:16:01 -0700

    lab1 fin

--- lab1.py
+++ lab1.py
@@ -5,3 +5,6 @@ print(requests.__version__)
 
 # get Google homepage
 print(requests.get("http://google.com"))
+
+response = requests.get("https://raw.githubusercontent.com/7bw/CMPUT404-Lab1/master/lab1.py")
+print(response.text)
